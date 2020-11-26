cf03276f4c2cba1a14a02711400749e3299ff150
Wolfgang Erlenkoetter <wolfgang.erlenkoetter@controlware.de>
2020-11-26 10:22:58 +0000

    Added second tag

--- main.tf
+++ main.tf
@@ -125,7 +125,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "devops"
+    Department = "devops",
+    Billable = true
   }
 }
 
